388efcdd7ce9bcb3106ac3ca8252a7067161c43e
Leila Skinner <beileiwang@gmail.com>
2020-03-27 16:15:16 -0500

    get sub muitply divide created and tested

--- server/server.js
+++ server/server.js
@@ -18,9 +18,27 @@ app.use(bodyParser.json());
 
 app.use(express.static("server/public"));
 
-// get
+// get addition result
 app.get("/addition", (req, res) => {
   let result = { result: addResult(num1, num2) };
+  res.send(result);
+});
+
+// get subtraction result
+app.get("/subtraction", (req, res) => {
+  let result = { result: subResult(num1, num2) };
+  res.send(result);
+});
+
+// get multiplication result
+app.get("/multiplication", (req, res) => {
+  let result = { result: multiplyResult(num1, num2) };
+  res.send(result);
+});
+
+// get division result
+app.get("/division", (req, res) => {
+  let result = { result: dividedResult(num1, num2) };
   res.send(result);
 });
 
