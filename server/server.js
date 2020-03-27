a225e612d3a8ca25ec6c4931889edce379179b0b
Leila Skinner <beileiwang@gmail.com>
2020-03-27 16:44:38 -0500

    POST created and assign result based on operator

--- server/server.js
+++ server/server.js
@@ -10,37 +10,73 @@ const dividedResult = require("./modules/division.js");
 const app = express();
 const PORT = 5000;
 // set default value for test
-let num1 = 20;
-let num2 = 5;
+let num1 = 0;
+let num2 = 0;
+let result = {};
+let operator = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("server/public"));
 
-// get addition result
-app.get("/addition", (req, res) => {
-  let result = { result: addResult(num1, num2) };
+// post numbers from client side
+app.post("/numbers", (req, res) => {
+  const numbers = req.body;
+  //below is the format the numbers will be send to the server
+  //{
+  //     num1: 20,
+  //     num2: '5
+  //     operator: ""
+  // }
+  num1 = numbers.num1;
+  num2 = numbers.num2;
+  operator = numbers.operator;
+  console.log(
+    `First Number: ${num1}, Second Number: ${num2}, Operator: ${operator}`
+  );
+  if (operator === "+") {
+    result = { result: addResult(num1, num2) };
+  } else if (operator === "-") {
+    result = { result: subResult(num1, num2) };
+  } else if (operator === "*") {
+    result = { result: multiplyResult(num1, num2) };
+  } else if (operator === "/") {
+    result = { result: dividedResult(num1, num2) };
+  }
+  console.log(result);
+
+  res.sendStatus(201);
+});
+
+// get result
+app.get("/result", (req, res) => {
   res.send(result);
 });
+
+// get addition result
+// app.get("/addition", (req, res) => {
+//   let result = { result: addResult(num1, num2) };
+//   res.send(result);
+// });
 
 // get subtraction result
-app.get("/subtraction", (req, res) => {
-  let result = { result: subResult(num1, num2) };
-  res.send(result);
-});
+// app.get("/subtraction", (req, res) => {
+//   let result = { result: subResult(num1, num2) };
+//   res.send(result);
+// });
 
 // get multiplication result
-app.get("/multiplication", (req, res) => {
-  let result = { result: multiplyResult(num1, num2) };
-  res.send(result);
-});
+// app.get("/multiplication", (req, res) => {
+//   let result = { result: multiplyResult(num1, num2) };
+//   res.send(result);
+// });
 
-// get division result
-app.get("/division", (req, res) => {
-  let result = { result: dividedResult(num1, num2) };
-  res.send(result);
-});
+// // get division result
+// app.get("/division", (req, res) => {
+//   let result = { result: dividedResult(num1, num2) };
+//   res.send(result);
+// });
 
 // runs server
 app.listen(PORT, (req, res) => {
