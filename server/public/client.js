2fe673a043cb2df0b4cfcfeea894d00c5de9bde1
Leila Skinner <beileiwang@gmail.com>
2020-03-28 13:37:04 -0500

    function to check operator works

--- server/public/client.js
+++ server/public/client.js
@@ -4,6 +4,8 @@ function init() {
   console.log("READY");
   // add event handler
   $(".js-btn-equal").on("click", submitNumbers);
+  // add event handler for operators
+  $(".js-calculator-input").on("click", checkOperator);
 }
 
 // event handler
@@ -14,4 +16,16 @@ function submitNumbers() {
     num2: $(".js-input-num2").val(),
     operator: ""
   };
+}
+
+// find the correct operator key clicked
+function checkOperator(event) {
+  const target = event.target;
+  if (target.matches("button") != true) {
+    return;
+  }
+  if (target.classList.contains("js-operator")) {
+    console.log("OPERATOR: ", target.value);
+    return;
+  }
 }
