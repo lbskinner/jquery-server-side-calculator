ec43a7660c0c81252fe5657633f9cf11319246e5
Leila Skinner <beileiwang@gmail.com>
2020-03-28 12:17:44 -0500

    init function created and submitNumbers called when equal sign button clicked

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,12 @@
-console.log("js");
+$(document).ready(init);
+
+function init() {
+  console.log("READY");
+  // add event handler
+  $(".js-btn-equal").on("click", submitNumbers);
+}
+
+// event handler
+function submitNumbers() {
+  console.log("EQUAL BUTTON CLICKED");
+}
