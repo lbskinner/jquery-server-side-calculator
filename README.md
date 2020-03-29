5b40c2433a61bf694e944367d4caffc572564af5
Leila Skinner <beileiwang@gmail.com>
2020-03-29 17:55:50 -0500

    split out render numbers, decimal and operator functions

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 
 ## Description
 
-For this challenge, I created a calculator and all of the logic and/or calculations are implemented on the server.
+For this challenge, I created a calculator. All of the logic and/or calculations are implemented on the server.
 
-It looks and acts like a regulator calculator. A user pressed the number keys for the first number, press an operator key and then press the number leys for the second number. The numbers and the operations are displayed in the calculator display filed in the order the keys were pressed. When the "=" button is pressed, the numbers selected and the operator selected are stored in an object and sent to the server. The server receives this information to calculate the result, store this result in an array and send this result to the front end. The front end receives the result and display the result on the page along with all of the previous calculations performed below the calculation.
+It looks and acts like a regular calculator. A user presses the number keys for the first number, presses an operator key and then presses the number keys for the second number. The numbers and the operations are displayed in the calculator display in the order the keys were pressed. When the "=" button is pressed, the numbers selected and the operator selected are stored in an object and sent to the server. The server receives this information to calculates the result, stores this result in an array and sends this result to the front end. The front end receives the result and displays the result on the page along with all of the previous calculations performed below the calculator.
 
 Just like a regular calculator, the previous answer can be used to continue the calculation. Press the "C" button to clear the calculator display and previous calculation data.
 
-The user could also click on an calculation in the history list to re-run that calculation. The answer of that calculation is displayed on the calculator display. The user could use this answer as a starting point to perform additional calculations. Below the calculation and above the list of history calculations, there a "Clear History" button. When the "Clear History" button is pressed, a delete is sent to server to clear all history data.
+The user could also click on a calculation in the history list to re-run that calculation. The answer of that calculation is displayed on the calculator display. The user could use this answer as a starting point to perform additional calculations. Below the calculation and above the list of history calculations, there is a "Clear History" button. When the "Clear History" button is pressed, a DELETE is sent to server to clear all history data.
 
-On the server side I used node express and body-parser to POST (receive) and GET (send) data. On the front end, I used jQuery AJAX to POST (send) the data (values and operator selected) inputted by the user and GET (receive) the data (answer and historical operation data) and display them page.
+On the server side I used node express and body-parser to POST and GET data. On the front end, I used jQuery AJAX to POST the data (values and operator selected) inputted by the user and GET the data (answer and historical operation data) and display them on the page.
 
 ## Screen Shot
 
@@ -25,7 +25,7 @@ Below is a screen shot of the application.
 
 ## Installation
 
-You need to make sure you have node installed on your computer. then open up your terminal and run `npm install`.
+You need to make sure you have node installed on your computer, then open up your terminal and run `npm install`.
 
 ## Built With
 
--- server/public/client.js
+++ server/public/client.js
@@ -32,6 +32,12 @@ function reRunHistoryCalculation(event) {
   const historyCalculationIndex = $(this).data("index");
   console.log("RE-RUN CALC; ", historyCalculationIndex);
   answer = allOperations[historyCalculationIndex].result;
+  if (answer.toString().includes(".")) {
+    let numAfterDecimal = answer.toString().split(".");
+    if (numAfterDecimal[1].length > 7) {
+      answer = answer.toFixed(7);
+    }
+  }
   renderDisplay(answer);
 }
 
@@ -68,60 +74,25 @@ function checkKeys(event) {
   if (target.matches("button") != true) {
     return;
   }
-  // if event targe clicked has class js-operator us true
+  // if event target clicked has class js-operator is true
   if (target.classList.contains("js-operator")) {
     console.log("OPERATOR: ", target.value);
-    // set the value of the button clicked to variable operator
-    operator = target.value;
-    // if answer has a value and operator is being clicked,
-    // the answer from previous became num1 and num2 reset
-    if (answer && operator) {
-      num1 = answer.toString();
-      num2 = "";
-    }
-    // display num1 and variable on calculator display
-    renderDisplay(num1 + operator);
+    // assign and display operator sign to calculator display
+    renderOperatorToDisplay(target.value);
     return;
   }
   // if event target clicked has class js-number is true
   if (target.classList.contains("js-number")) {
     console.log("NUMBER: ", target.value);
-    // set the value of the button clicked to num1 or num2 based on the following condition
-    // if num1 does not have a value
-    if (!num1) {
-      // num1 equals the value of the button clicked
-      num1 = target.value;
-      //else if num1 has a value and the operator does not have a value
-    } else if (num1 && !operator) {
-      // num1 equals to num1 concatenate value of button clicked
-      num1 += target.value;
-      // else if num1 and operator have values and num2 does not have a value
-    } else if (num1 && operator && !num2) {
-      // num2 equals to the value of the button clicked
-      num2 = target.value;
-      // else if num1, operator and num2 all have values
-    } else if (num1 && operator && num2) {
-      // num2 equals to num1 concatenate value of button clicked
-      num2 += target.value;
-    }
-    // display num1, operator, and num2 on calculator display
-    renderDisplay(num1 + operator + num2);
+    // assign number values to num1 and num2 and display on calculator display
+    renderNumbersToDisplay(target.value);
     return;
   }
   // if event target clicked has class js-decimal is true
   if (target.classList.contains("js-decimal")) {
     console.log("DECIMAL: ", target.value);
-    // if num1 does not include a "." and operator has not been pressed
-    if (!num1.includes(".") && !operator) {
-      // concatenate the "." to num1
-      num1 += target.value;
-      renderDisplay(num1);
-      // else if num2 does not include a "."
-    } else if (!num2.includes(".")) {
-      // concatenate the "." to num2
-      num2 += target.value;
-      renderDisplay(num1 + operator + num2);
-    }
+    // add decimal to num1 and num2
+    renderDecimalToDisplay(target.value);
     return;
   }
 }
@@ -129,7 +100,6 @@ function checkKeys(event) {
 // API interactions
 
 // delete/clear all history
-
 function clearAllHistory() {
   $.ajax({
     method: "DELETE",
@@ -175,6 +145,7 @@ function getResult() {
           answer = answer.toFixed(7);
         }
       }
+      //   formatDisplayDecimal(answer);
       // display calculation result on DOM
       renderDisplay(answer);
       //get history from server to display on DOM
@@ -204,11 +175,60 @@ function getHistory() {
 
 // render to the DOM
 
-// render result to DOM, not longer used
+// render result to DOM, used for basic mode, no longer used for stretch
 // function renderResult(result) {
 //   $(".js-calculator-result").text(result);
 // }
 
+//render numbers to display
+function renderNumbersToDisplay(numValue) {
+  // if num1 does not have a value
+  if (!num1) {
+    // num1 equals the value of the button clicked
+    num1 = numValue;
+    //else if num1 has a value and the operator does not have a value
+  } else if (num1 && !operator) {
+    // num1 equals to num1 concatenate value of button clicked
+    num1 += numValue;
+    // else if num1 and operator have values and num2 does not have a value
+  } else if (num1 && operator && !num2) {
+    // num2 equals to the value of the button clicked
+    num2 = numValue;
+    // else if num1, operator and num2 all have values
+  } else if (num1 && operator && num2) {
+    // num2 equals to num1 concatenate value of button clicked
+    num2 += numValue;
+  }
+  // display num1, operator, and num2 on calculator display
+  renderDisplay(num1 + operator + num2);
+}
+//render operator to display
+function renderOperatorToDisplay(operatorValue) {
+  operator = operatorValue;
+  // if answer has a value and operator is being clicked,
+  // the answer from previous became num1 and num2 reset
+  if (answer && operator) {
+    num1 = answer.toString();
+    num2 = "";
+  }
+  // display num1 and variable on calculator display
+  renderDisplay(num1 + operator);
+}
+
+//render decimal to display
+function renderDecimalToDisplay(decimalValue) {
+  // if num1 does not include a "." and operator has not been pressed
+  if (!num1.includes(".") && !operator) {
+    // concatenate the "." to num1
+    num1 += decimalValue;
+    renderDisplay(num1);
+    // else if num2 does not include a "."
+  } else if (!num2.includes(".")) {
+    // concatenate the "." to num2
+    num2 += decimalValue;
+    renderDisplay(num1 + operator + num2);
+  }
+}
 // display inputs on calculator display
 function renderDisplay(num) {
   $(".js-calculator-display").val(num);
